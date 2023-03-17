2fd835f2790134e760c50560d25af9255ba11acf
aiamin <amin.mafn@gmail.com>
2023-03-17 22:30:50 +0330

    booklist project

--- react2/react2/src/index.js
+++ react2/react2/src/index.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import './index.css'
 
-function Greeting() {
-  return <h2>My first cimponent</h2>
-}
+const BookList = () => {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
 
-root.render(<Greeting />);
+const Image = () => (
+  <img src="https://m.media-amazon.com/images/P/0593236211.01._SCLZZZZZZZ_SX500_.jpg" width={200}/>
+);
+const Title = () => <h2>Everyday Grand: Soulful Recipes for Celebrating</h2>;
+const Author = () => <h2>Jocelyn Delk Adams</h2>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<BookList />);
