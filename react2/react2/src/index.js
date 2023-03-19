f0367f08c8305217ba6af85a956ea25073b03329
aiamin <amin.mafn@gmail.com>
2023-03-19 23:14:43 +0330

    some changes

--- react2/react2/src/index.js
+++ react2/react2/src/index.js
@@ -1,15 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import './index.css'
+import "./index.css";
 
 const BookList = () => {
   return (
-    <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-    </section>
+    <div>
+      <EventExample />
+      <section className="booklist">
+        <Book />
+        <Book />
+        <Book />
+      </section>
+    </div>
   );
+};
+
+const EventExample = () => {
+  return <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error saepe quisquam rerum natus facilis amet culpa, iusto dolor excepturi sunt illum, deleniti consequatur velit cum tenetur dicta eum enim! Odio.</h2>;
 };
 
 const Book = () => {
@@ -23,11 +30,14 @@ const Book = () => {
 };
 
 const Image = () => (
-  <img src="https://m.media-amazon.com/images/P/0593236211.01._SCLZZZZZZZ_SX500_.jpg" width={200}/>
+  <img
+    src="https://m.media-amazon.com/images/P/0593236211.01._SCLZZZZZZZ_SX500_.jpg"
+    alt="alternative"
+    width={200}
+  />
 );
 const Title = () => <h2>Everyday Grand: Soulful Recipes for Celebrating</h2>;
 const Author = () => <h2>Jocelyn Delk Adams</h2>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(<BookList />);
