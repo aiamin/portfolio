02133abb27be45c7d4f47589be1994c75d404b5e
aiamin <amin.mafn@gmail.com>
2023-03-28 15:43:50 +0330

    delete old files

--- react/hello-world/src/index.js
+++
@@ -1,6 +0,0 @@
-import ReactDOM from "react-dom";
-// import Product from "./components/product";
-import "bootstrap/dist/css/bootstrap.min.css";
-import NewFunction from "./components/new";
-
-ReactDOM.render(<NewFunction />, document.getElementById("root"));
