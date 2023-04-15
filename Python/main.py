ef3d59b41a14cbc2e0e590deef5973bde716d6a3
aiamin <amin.mafn@gmail.com>
2023-04-15 13:47:37 +0330

    end of project 01

--- Python/main.py
+++ Python/main.py
@@ -14,6 +14,30 @@ symbol_count = {
     "D" : 8
 }
 
+symbol_value = {
+    "A" : 5,
+    "B" : 4,
+    "C" : 3,
+    "D" : 2
+}
+
+
+def check_winnning(columns, lines, bet, values):
+    winnings = 0
+    winning_lines = []
+    for line in range(lines):
+        symbol = columns[0][line]
+        for column in columns:
+            symbol_to_check = column[line]
+            if symbol != symbol_to_check:
+                break
+        else:
+            winnings += values[symbol] * bet
+            winning_lines.append(line + 1)
+
+    return winnings, winning_lines
+
+
 def get_slot_machine_spin (rows, cols, symbols):
     all_symbols = []
     for symbol, symbol_count in symbols.items():
@@ -40,7 +64,9 @@ def print_slot_machine(columns):
             if i != len(columns) - 1:
                 print(column[row], end=" | ")
             else:
-                print(column[row])
+                print(column[row], end="")
+
+        print()
 
 
 def deposit():
@@ -85,10 +111,7 @@ def get_bet():
 
     return amount    
 
-
-
-def main():
-    balance = deposit()
+def spin(balance):
     lines = get_nember_of_lines()
     while True:
         bet = get_bet()
@@ -104,5 +127,21 @@ def main():
 
     print_slot_machine(slots)
 
+    winnings, winning_lines = check_winnning(slots, lines, bet, symbol_value)
+    print(f"You won ${winnings}.")
+    print(f"You won on lines:", *winning_lines)
+    return winnings - total_bet
+
+
+def main():
+    balance = deposit()
+    while True:
+        print(f"Current balance is ${balance}")
+        answer = input("Press enter to play (q to quit).")
+        if answer == "q":
+            break
+        balance += spin(balance)
+            
+    print(f"You left with ${balance}")
 
 main()
